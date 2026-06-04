f099c196e19d4807867668d5a7f28d0f3df6fbd5
Jasper Chan <jasperchan515@gmail.com>
2026-06-04 16:10:16 -0700

    bugfix: vendor dataclasses module
    * feature: start creating context menu action

--- src/lrclib_picard/__init__.py
+++ src/lrclib_picard/__init__.py
@@ -10,13 +10,27 @@ PLUGIN_LICENSE_URL = "https://spdx.org/licenses/MIT.html"
 PLUGIN_VERSION = "2.2.3"
 PLUGIN_API_VERSIONS = ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7"]
 
+import sys
+import os
+from typing import Union, List
+
+# HACK: Picard (at least on Windows) seems to be missing some stuff from
+# python stdlib
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "vendor"))
+
 from picard import log
 from picard.ui.itemviews import (
     BaseAction,
     register_track_action,
+    register_album_action,
+    register_file_action,
 )
+from picard.track import Track
+from picard.album import Album
+from picard.file import File
 
 from .lrclib_py import LrcLib
+from .util import flatten_to_files
 
 
 def get_client():
@@ -27,13 +41,17 @@ def get_client():
 class LrcLibLyricsGet(BaseAction):
     NAME = "Test Action"
 
-    def callback(self, objs):
-        import traceback
+    def callback(self, objs: List[Union[Album, Track, File]]):
+        files = flatten_to_files(objs)
+        for obj in files:
+            if isinstance(obj, Album):
+                log.info(f"Album: {obj}, {obj.metadata.get('title')}")
+            elif isinstance(obj, Track):
+                log.info(f"Track: {obj}, {obj.metadata.get('title')}")
+            elif isinstance(obj, File):
+                log.info(f"File: {obj}, {obj.filename}")
 
-        log.info("=====OBJS")
-        log.info(objs)
-        log.info("======TRACEBACK")
-        log.info(traceback.format_stack())
 
-
+register_album_action(LrcLibLyricsGet())
 register_track_action(LrcLibLyricsGet())
+register_file_action(LrcLibLyricsGet())
---
+++ src/lrclib_picard/util.py
@@ -0,0 +1,24 @@
+from typing import Union, Iterable, List
+
+from picard.track import Track
+from picard.album import Album
+from picard.file import File
+
+_item = Union[Album, Track, File]
+
+
+def flatten_to_files(objs: Union[Iterable[_item], _item]) -> List[File]:
+    if not isinstance(objs, Iterable):
+        objs = [objs]
+    out = []
+    for obj in objs:
+        if isinstance(obj, File):
+            out.append(obj)
+        elif isinstance(obj, Track):
+            if obj.linked_files:
+                out.extend(obj.linked_files)
+        elif isinstance(obj, Album):
+            for track in obj.tracks:
+                if track.linked_files:
+                    out.extend(track.linked_files)
+    return out
